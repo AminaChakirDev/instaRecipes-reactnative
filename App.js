9490df57cfb33680f3857fdd5a5f753744850aaa
AminaChakirDev <amina.aitm@gmail.com>
2021-04-09 10:32:08 +0200

    Customize BottomTabNavigator

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import Home from "./Components/Home";
 import Recipe from "./Components/Recipe";
 import Logo from "./Components/Logo"
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Favorites from './Components/Favorites';
 import MyAccount from './Components/MyAccount';
 import Search from './Components/Search';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
 
 
 const HomeStack = createStackNavigator();
@@ -123,7 +124,51 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Home':
+                iconName = focused
+                  ? 'home'
+                  : 'home';
+                break;
+
+              case 'Search':
+                iconName = focused
+                  ? 'search'
+                  : 'search';
+                break;
+
+              case 'Favorites':
+                iconName = focused
+                  ? 'heart'
+                  : 'heart-o';
+                break;
+
+              case 'MyAccount':
+                iconName = focused
+                  ? 'user'
+                  : 'user';
+                break;
+
+              default:
+                'home'
+            }
+
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#FF3453',
+          activeBackgroundColor: '#FFFFFF',
+          inactiveTintColor: '#2c001b',
+          inactiveBackgroundColor: '#FFFFFF',
+          showLabel: false,
+        }}
+      >
         <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Search" component={SearchStackScreen} />
         <Tab.Screen name="Favorites" component={FavoritesStackScreen} />
