7cf50a18757a4323c1b6a3942f3a87312bf00b84
AminaChakirDev <amina.aitm@gmail.com>
2021-04-09 09:46:29 +0200

    Add FavoritesPage to bottom tab

--- App.js
+++ App.js
@@ -4,39 +4,77 @@ import Home from "./Components/Home";
 import Recipe from "./Components/Recipe";
 import Logo from "./Components/Logo"
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Favorites from './Components/Favorites';
 
-export default function App() {
 
-  const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
 
+function HomeStackScreen() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { elevation: 0, shadowColor: 'transparent' },
-          cardStyle: { background: '#fff' },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: true,
-            headerTitle: props => <Logo {...props} />,
-            headerBackTitle: ' '
-          }}
-        />
-        <Stack.Screen
-          name="Recipe"
-          component={Recipe}
-          options={{
+    <HomeStack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: { elevation: 0, shadowColor: 'transparent' },
+        cardStyle: { background: '#fff' },
+      }}
+    >
+      <HomeStack.Screen
+        name="Home"
+        component={Home}
+        options={{
           headerShown: true,
           headerTitle: props => <Logo {...props} />,
           headerBackTitle: ' '
-          }}
-        />
-      </Stack.Navigator>
+        }}
+      />
+      <HomeStack.Screen
+        name="Recipe"
+        component={Recipe}
+        options={{
+          headerShown: true,
+          headerTitle: props => <Logo {...props} />,
+          headerBackTitle: ' '
+        }}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+
+const FavoritesStack = createStackNavigator();
+
+function FavoritesStackScreen() {
+  return (
+    <FavoritesStack.Navigator
+      initialRouteName="Favorites"
+      screenOptions={{
+        headerStyle: { elevation: 0, shadowColor: 'transparent' },
+        cardStyle: { background: '#fff' },
+      }}
+    >
+      <FavoritesStack.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          headerShown: true,
+          headerTitle: props => <Logo {...props} />,
+          headerBackTitle: ' '
+        }}
+      />
+    </FavoritesStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Favorites" component={FavoritesStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
