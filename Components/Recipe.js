5924b3e5e1af483d07a84c96ddcee9cc17fb3358
AminaChakirDev <amina.aitm@gmail.com>
2021-04-09 10:44:14 +0200

    Add style to recipe component

--- Components/Recipe.js
+++ Components/Recipe.js
@@ -14,15 +14,15 @@ export default function Recipe({ route }) {
             <WebView
                 originWhitelist={['*']}
                 source={{ html: `<iframe src=${recipe.item.instaLink} width="970" height="1075" frameborder="0" scrolling="no" allowtransparency="true"></iframe>` }}
-                style={{ marginTop: 20 }}
+                style={{ marginTop: 10 }}
             />
             <ScrollView>
-                <Text>Ingrédients</Text>
-                <Text>flatlist liste ingrédient</Text>
-                <Text>Préparation</Text>
-                <Text>Fouetter la farine, les sucres, le sel, la vanille et le lait et l’eau. Ajouter l’huile et les oeufs sans cesser de fouetter jusqu’à ne plus avoir de grumeaux.</Text>
-                <Text>Cuisson</Text>
-                <Text>Chauffer une poêle et commencer la cuisson .</Text>
+                <Text style={styles.subTitle}>Ingrédients</Text>
+                <Text style={styles.content}>flatlist liste ingrédient</Text>
+                <Text style={styles.subTitle}>Préparation</Text>
+                <Text style={styles.content}>Fouetter la farine, les sucres, le sel, la vanille et le lait et l’eau. Ajouter l’huile et les oeufs sans cesser de fouetter jusqu’à ne plus avoir de grumeaux.</Text>
+                <Text style={styles.subTitle}>Cuisson</Text>
+                <Text style={styles.content}>Chauffer une poêle et commencer la cuisson.</Text>
             </ScrollView>
         </View>
     );
@@ -37,5 +37,18 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#2C001B',
         fontFamily: 'Helvetica Neue',
-    }
+        fontSize: 20,
+        marginTop: 30
+    },
+    subTitle: {
+        color: '#2C001B',
+        fontFamily: 'Helvetica Neue',
+        fontSize: 20,
+        marginTop: 20
+    },
+    content: {
+        color: '#2C001B',
+        fontFamily: 'Helvetica Neue',
+        fontSize: 20,
+    },
 });
